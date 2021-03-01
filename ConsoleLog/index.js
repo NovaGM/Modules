bfc88a928a92e10951b8bb42d0c6d1ce3e7ac2c7
Lexi Sother <lexisoth2005@gmail.com>
2021-03-01 17:25:42 +0000

    Added ConsoleLog module

---
+++ ConsoleLog/index.js
@@ -0,0 +1,14 @@
+import showToast from '@goosemod/toast';
+
+export default {
+  goosemodHandlers: {
+    onImport: () => {
+      console.log('This is a log from ConsoleLog');
+      showToast('This is a toast from ConsoleLog');
+    },
+    remove: () => {
+      console.log('ConsoleLog has been uninstalled');
+      showToast('ConsoleLog has been uninstalled');
+    },
+  },
+};
